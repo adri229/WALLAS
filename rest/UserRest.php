0a0188dd47ff5f23893dee2316728848454f3dfd
adri229 <adricelixfernandez@gmail.com>
2016-02-25 19:39:41 +0100

    update method pending refactor

--- rest/UserRest.php
+++ rest/UserRest.php
@@ -34,7 +34,7 @@ class UserRest extends BaseRest
         } 
     }
     
-    
+    /*
     public function update($login, $data) 
     {
     	$currentUser = parent::authenticateUser();
@@ -65,32 +65,132 @@ class UserRest extends BaseRest
         }
         
     }
-    
+    */
+
+    private function update($login, $data)
+    {
+        $user = $this->userDAO->findByID($login);
+        if ($user == NULL) {
+            header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
+            echo("User with id ".$login." not found");
+        }
+        return $user;
+    }
+
     public function updatePassword ($login,$password)
     {
-    	$currentUser = parent::authenticateUser();
-    	if ($login != $currentUser->getLogin()) {
-    		header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
-    		echo("You are not authorized to access this resource");
-    	}
-    	
-    	$user = $this->userDAO->findByID($login);
-    	if ($user == NULL) {
-    		header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
-    		echo("User with id ".$login." not found");
-    	}
-    	$user->setPassword($password->password);
+        $currentUser = parent::authenticateUser();
+        if ($login != $currentUser->getLogin()) {
+            header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+            echo("You are not authorized to access this resource");
+            //return;
+        }
 
-    	 try {
+        $user = $this->update($login,$password);
+        $user->setPassword($password->password);
+
+         try {
             //$user->validate();
             $this->userDAO->updatePassword($user);
             header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
-            //header("Location: ".$_SERVER['REQUEST_URI']);
         } catch (Exception $e) {
             http_response_code(400);
             echo(json_encode($e->getErrors()));
         }
     }
+
+    public function updateEmail ($login, $email)
+    {
+        $currentUser = parent::authenticateUser();
+        if ($login != $currentUser->getLogin()) {
+            header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+            echo("You are not authorized to access this resource");
+            return;
+        }
+
+        $user = $this->update($login,$email);
+        $user->setEmail($email->email);
+
+         try {
+            //$user->validate();
+            $this->userDAO->updateEmail($user);
+            header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+        } catch (Exception $e) {
+            http_response_code(400);
+            echo(json_encode($e->getErrors()));
+        }
+    }
+
+
+    public function updatePhone ($login, $phone)
+    {
+        $currentUser = parent::authenticateUser();
+        if ($login != $currentUser->getLogin()) {
+            header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+            echo("You are not authorized to access this resource");
+            return;
+        }
+
+        $user = $this->update($login, $phone);
+        $user->setPhone($phone->phone);
+
+         try {
+            //$user->validate();
+            $this->userDAO->updatePhone($user);
+            header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+        } catch (Exception $e) {
+            http_response_code(400);
+            echo(json_encode($e->getErrors()));
+        }
+    }
+
+
+    public function updateAddress ($login, $address)
+    {
+        $currentUser = parent::authenticateUser();
+        if ($login != $currentUser->getLogin()) {
+            header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+            echo("You are not authorized to access this resource");
+            return;
+        }
+
+        $user = $this->update($login,$address);
+        $user->setAddress($address->address);
+
+         try {
+            //$user->validate();
+            $this->userDAO->updateAddress($user);
+            header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+        } catch (Exception $e) {
+            http_response_code(400);
+            echo(json_encode($e->getErrors()));
+        }
+    }
+
+    public function updateCountry ($login, $country)
+    {
+        $currentUser = parent::authenticateUser();
+        if ($login != $currentUser->getLogin()) {
+            header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+            echo("You are not authorized to access this resource");
+            return;
+        }
+
+        $user = $this->update($login,$country);
+        $user->setCountry($country->country);
+
+         try {
+            //$user->validate();
+            $this->userDAO->updateCountry($user);
+            header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+        } catch (Exception $e) {
+            http_response_code(400);
+            echo(json_encode($e->getErrors()));
+        }
+    }
+
+
+
     
     public function delete ($login) 
     {
@@ -162,6 +262,10 @@ $userRest = new UserRest();
 	->map("POST", "/users", array($userRest, "create"))
 	->map("PUT", "/users/$1", array($userRest, "update"))
     ->map("PUT", "/users/$1/password", array($userRest, "updatePassword"))
+    ->map("PUT", "/users/$1/email", array($userRest, "updateEmail"))
+    ->map("PUT", "/users/$1/phone", array($userRest, "updatePhone"))
+    ->map("PUT", "/users/$1/address", array($userRest, "updateAddress"))
+    ->map("PUT", "/users/$1/country", array($userRest, "updateCountry"))
 	->map("DELETE", "/users/$1", array($userRest, "delete"));
 	
 
