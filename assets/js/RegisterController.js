a293e18f13414500cd16209ec6bb5f1d38722cc9
adri229 <adricelixfernandezgmail.com>
2016-04-25 17:40:39 +0200

    changes in register user

--- assets/js/RegisterController.js
+++ assets/js/RegisterController.js
@@ -3,6 +3,7 @@
 var wallas = angular.module('wallasApp');
 
 wallas.controller('RegisterController', ['$scope','UserService', function ($scope, UserService) {
+    $scope.showHints = true;
     $scope.register = function() {
       	UserService.create($scope.user).then(
        		function(response) {
