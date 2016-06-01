b0cd640b363a2075c465673d977b3263698f0f6f
adri229 <adricelixfernandez@gmail.com>
2016-06-01 17:17:53 +0200

    create sql query for chart 3

--- database/spending.sql
+++ database/spending.sql
@@ -1,10 +1,6 @@
 SELECT s.idSpending as 'spending.id',
-		s.dateSpending as 'spending.date',
-		s.quantity as 'spending.quantity',
-		s.name as 'spending.name',
-		s.owner as 'spending.owner',
+		SUM(s.quantity) as 'spending.quantity',
 		t.idType as 'type.id',
-		t.name as 'type.name',
-		t.owner as 'type.owner'
-FROM SPENDING s LEFT JOIN TYPE_SPENDING ts  ON s.idSpending = ts.spending LEFT JOIN TYPE t on ts.type = t.idType 
-WHERE s.owner = 'adri229' ORDER BY 'spending.id';
+		t.name as 'type.name'
+FROM SPENDING s LEFT JOIN TYPE_SPENDING ts  ON s.idSpending = ts.spending LEFT JOIN TYPE t on ts.type = t.idType  
+WHERE s.owner = 'adri229' AND ts.spending IS NOT NULL GROUP BY ts.type ORDER BY 'spending.id';
